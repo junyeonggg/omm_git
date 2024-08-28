842df3f6a1687b2dc1db33192f5b7614d142ba8a
admin <admin@192.168.56.1>
2024-08-28 15:34:17 +0900

    asd

--- omm/src/main/java/com/omm/controller/MemberController.java
+++ omm/src/main/java/com/omm/controller/MemberController.java
@@ -12,5 +12,7 @@ public class MemberController {
         System.out.println("origin2");
         System.out.println("origin3");
         System.out.println("origin333333");
+        System.out.println("준영");
+        System.out.println("준영");
 	}
 }
