4af62f6507287e759e5d1ddaaee9b5c9fa27e263
admin <jongmoon8814@gmail.com>
2024-08-28 14:57:32 +0900

    안녕

--- omm/src/main/java/com/omm/controller/MemberController.java
+++ omm/src/main/java/com/omm/controller/MemberController.java
@@ -10,5 +10,6 @@ public class MemberController {
 		System.out.println("바이222");
         System.out.println("origin");
         System.out.println("origin2");
+        System.out.println("origin3");
 	}
 }
