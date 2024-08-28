7754611f440639c888ee166b79cafbab5199b5f0
admin <admin@192.168.56.1>
2024-08-28 14:31:12 +0900

    컨트롤러 폴더

---
+++ omm/src/main/java/com/omm/OmmApplication.java
@@ -0,0 +1,13 @@
+package com.omm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmmApplication.class, args);
+	}
+
+}
