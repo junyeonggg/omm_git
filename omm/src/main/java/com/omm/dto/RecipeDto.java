10ec2ce996a7c7b01ea01977580dac48f0a9a0de
admin <jongmoon8814@gmail.com>
2024-08-28 17:21:44 +0900

    안녕

--- omm/src/main/java/com/omm/dto/RecipeDto.java
+++ omm/src/main/java/com/omm/dto/RecipeDto.java
@@ -1,10 +1,10 @@
-package com.omm.dto
+package com.omm.dto;
 
-import lombok.AllArgsConstructor
-import lombok.Getter
-import lombok.NoArgsConstructor
-import lombok.Setter
-import lombok.ToString
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @ToString
 @AllArgsConstructor
