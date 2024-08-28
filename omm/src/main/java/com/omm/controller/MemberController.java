460496f758c8d92cdad23c1477dd5cb955a1ff88
admin <admin@192.168.56.1>
2024-08-28 15:41:05 +0900

    초기

--- omm/src/main/java/com/omm/controller/MemberController.java
+++ omm/src/main/java/com/omm/controller/MemberController.java
@@ -2,21 +2,6 @@ package com.omm.controller;
 
 public class MemberController {
 	void method() {
-		System.out.println("하이");
-        System.out.println("안녕");
-        System.out.println("바이");
-        System.out.println("바이2");
-        System.out.println("안녕");
-		System.out.println("바이222");
-        System.out.println("origin");
-        System.out.println("origin2");
-        System.out.println("origin3");
-        System.out.println("origin333333");
-        System.out.println("origin333333");
-        System.out.println("origin333333");
-        System.out.println("origin333333");
-        
-        System.out.println("준영");
-        System.out.println("준영");
+
 	}
 }
