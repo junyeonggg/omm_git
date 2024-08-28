d65546c96d21babf0c01420d5ae397310ddb7b27
admin <jongmoon8814@gmail.com>
2024-08-28 17:48:49 +0900

    안녕

--- omm/src/main/java/com/omm/dto/CommentDto.java
+++ omm/src/main/java/com/omm/dto/CommentDto.java
@@ -8,4 +8,12 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 public class CommentDto {
+    private int comment_id;
+    private String user_id;
+    private String comment_content;
+    private String comment_create_date;
+    private int target_id;
+    private int parent_comment_id;
+    private double comment_rating;
+    private int reference_type;
 }
--- omm/src/main/java/com/omm/dto/InquireDto.java
+++ omm/src/main/java/com/omm/dto/InquireDto.java
@@ -8,4 +8,9 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 public class InquireDto {
+    private int inquire_id;
+    private String user_id;
+    private String inquire_title;
+    private String inquire_content;
+    private String inquire_create_date;
 }
