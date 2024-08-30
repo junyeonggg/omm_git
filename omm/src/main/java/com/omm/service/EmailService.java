63fa7e71cc794e9e51c74deddf26ca2fc1d199c3
admin <admin@192.168.56.1>
2024-08-30 10:53:53 +0900

    Merge remote-tracking branch 'origin/master'

--- omm/src/main/java/com/omm/service/EmailService.java
+++ omm/src/main/java/com/omm/service/EmailService.java
@@ -3,6 +3,7 @@ package com.omm.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     private final JavaMailSender email_sender;
 
+    @Async
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("whdans1542@naver.com"); // 발송자 이메일 주소
@@ -18,4 +20,30 @@ public class EmailService {
         message.setText(text);
         email_sender.send(message);
     }
+}
+
+
+
+@Service
+public class EmailServiceImpl implements EmailService {
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Override
+	public boolean sendMail(EmailDto dto) {
+		boolean flag = true;
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setSubject(dto.getSubject());
+		message.setTo(dto.getTo());
+		message.setText(dto.getText());
+		System.out.println(message.toString());
+		try {
+			javaMailSender.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+			flag=false;
+		}
+		return flag;
+	}
+
 }
--- omm/src/main/java/com/omm/service/UserSecurityService.java
+++ omm/src/main/java/com/omm/service/UserSecurityService.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
