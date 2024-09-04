0a7cdc6ca9e34d5bdb1b8f6ebd9aa26abaf358f0
yoonn2 <bling9356@gmail.com>
2024-09-04 16:52:09 +0900

    상품 상세페이지 댓글창, 별점 추가 기능 미구현

--- omm/src/main/java/com/omm/dto/CommentDto.java
+++ omm/src/main/java/com/omm/dto/CommentDto.java
@@ -6,11 +6,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
+
 @ToString
 @AllArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
 public class CommentDto {
     private int comment_id;
     private String user_id;
@@ -20,4 +21,11 @@ public class CommentDto {
     private int parent_comment_id;
     private double comment_rating;
     private int reference_type;
+
+    public CommentDto(){
+        LocalDate now = LocalDate.now();
+        this.comment_create_date = now.toString();
+
+    }
+
 }
